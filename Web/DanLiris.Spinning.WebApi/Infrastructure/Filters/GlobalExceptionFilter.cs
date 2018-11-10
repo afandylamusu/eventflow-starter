da1b0fbdfad3190dbad188bcc79f756bd15505e2
afandy lamusu <afandy.lamusu@live.co.uk>
2018-11-10 13:04:49 +0700

    Add GlobalException

--- Web/DanLiris.Spinning.WebApi/Infrastructure/Filters/GlobalExceptionFilter.cs
+++ Web/DanLiris.Spinning.WebApi/Infrastructure/Filters/GlobalExceptionFilter.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DanLiris.Spinning.WebApi.Infrastructure.Filters
 {
-    public class GlobalExceptionFilter
+    public class GlobalExceptionFilter : IExceptionFilter
     {
+        public void OnException(ExceptionContext context)
+        {
+            
+        }
     }
 }
